c5c157a33bd479cb34f968d9a818ca52286f2c65
soohyeon kim <soohyeon.kim@student.uni-luebeck.de>
2024-10-25 00:19:06 +0200

    Initial commit

---
+++ src/main/java/com/example/cicd_test/CicdTestApplication.java
@@ -0,0 +1,21 @@
+package com.example.cicd_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdTestApplication.class, args);
+	}
+
+	@RestController
+	class HelloConteroller{
+		@GetMapping("/")
+		public String hello(){
+			return "Hello, CI/CD with Spring Boot!";
+		}
+	}
+
+}
